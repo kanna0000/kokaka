5b73c2f4a5ee957256e960a283e0b6c9e40bbe51
kanna0000 <kanna@vivaldi.net>
2018-09-30 20:37:33 +0900

    Create account and Login

---
+++ app/controllers/user_session_controller.rb
@@ -0,0 +1,14 @@
+class UserSessionController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if @user = login(params[:username], params[:password])
+      redirect_back_or_to user_path(@user), notice: 'Login successful'
+    else
+      flash.now[:alert] = 'Login failed'
+      render action: 'new'
+    end
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        login(user_params[:username], user_params[:password])
+        format.html {
+          redirect_to user_path(@user), notice: 'User was successfully created.'
+        }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
